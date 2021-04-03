06b952340100b74949cb1dfe9cfa0198cded860e
Zishan <amanzishan.az@gmail.com>
2021-04-03 17:23:11 +0530

    update

---
+++ week-1/countingvalleys.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+def countingValleys(steps, path):
+
+
+
+'''
+Test case 1:
+8
+UDDDUDUU
+expected output : 1
+
+Test case 2:
+12
+DDUUDDUDUUUD
+expected output : 2
+'''
+
+
+#countingValleys(8, "UDDDUDUU")
+#countingValleys(12,"DDUUDDUDUUUD")
