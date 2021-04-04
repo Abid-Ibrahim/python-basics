aff3957fdaa235db6d4409df0c9e8f9db3dd0c86
Zishan <amanzishan.az@gmail.com>
2021-04-04 17:24:31 +0530

    Update day2-excercise.py

--- week-1/day2-excercise.py
+++ week-1/day2-excercise.py
@@ -7,7 +7,7 @@ if a or b and c:
 else:
     print ("output 2")
 
-
+'''
 
 a = True
 b = False
@@ -21,3 +21,18 @@ elif not a or b or not b and a:
     print (3)
 else:
     print (4)
+
+
+count = 1 
+  
+def doThis():
+  
+    global count
+  
+    for i in (1, 2, 3): 
+        count += 1
+  
+doThis()
+  
+print (count)
+'''
